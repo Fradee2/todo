8c1467a1b3366d50473ee5d28a539c0c853015ce
Fradee2 <ysikov.exceedteam@gmail.com>
2021-03-18 15:33:03 +0300

    Creat task

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-
-let input_value = document.getElementById('input_value').value;
+let i=0
 let task={};
 let main_footer = document.getElementById('main_footer');
-function creat_new_task(task){
-    main_footer.insertAdjacentHTML('afterend', `<div><p id="main_footer__name">${input_value}</p><input type="button"></input></div>`);
-}
-
-
 button.onclick=function(){
-    creat_new_task(task);
-    task={
-        task:input_value,
-        performed:false,
+    let input_value = document.getElementById("input_value").value;
+    id="${i}"
+    main_footer.insertAdjacentHTML('afterend', `<div id="${i}"><p id="main_footer__name ">${input_value}</p><button onClick="deletes(this)"  id="button_delete${i}" ></button></div>`);
+    i++;
+    // task={
+    //     task:input_value,
+    //     performed:false,
+    // }
     }
-    }
+function deletes(obj){
+    let a=obj.parentNode.id;
+    a.remuve();
+    
+
+}
