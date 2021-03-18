5d3e78b60d91bf19c76ba7e5076cbdf7e3cb8964
Fradee2 <ysikov.exceedteam@gmail.com>
2021-03-18 13:58:16 +0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const input_value = document.getElementById(input_value);
+
+let input_value = document.getElementById('input_value').value;
+let task={};
+let main_footer = document.getElementById('main_footer');
+function creat_new_task(task){
+    main_footer.insertAdjacentHTML('afterend', `<div><p id="main_footer__name">${input_value}</p><input type="button"></input></div>`);
+}
+
+
+button.onclick=function(){
+    creat_new_task(task);
+    task={
+        task:input_value,
+        performed:false,
+    }
+    }
