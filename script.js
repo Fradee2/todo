044a40f855aefdbea7f245db2ffd0b1e878fe7a3
Fradee2 <ysikov.exceedteam@gmail.com>
2021-03-19 14:26:42 +0300

    R

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ let button_delete;
 
 button_create.onclick=function(){
     let value=document.getElementById("input_value").value;
-    if(i>0){
-        for(let r=1;r!=tasks.length;r++){
-            if (tasks[r]==undefined){
-                main_footer.insertAdjacentHTML('afterbegin',`<div class="ne_gotovo" id="processing${r}"><p>${value}</p><button id="${r}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${r}">Удалить</button></>`);
-                break;
-            }
-        }
-    }else{
-    main_footer.insertAdjacentHTML('afterbegin',`<div class="ne_gotovo" id="processing${i}"><p>${value}</p><button id="${i}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${i}">Удалить</button></>`);
-    }
+    // if(i>0){
+    //     for(let r=1;r!=tasks.length;r++){
+    //         if (tasks[r]==undefined){
+    //             main_footer.insertAdjacentHTML('afterbegin',`<div class="ne_gotovo text" id="processing${r}"><p>${value}</p><button id="${r}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${r}">Удалить</button></>`);
+    //             break;
+    //         }
+    //     }
+    // }else{
+    main_footer.insertAdjacentHTML('afterbegin',`<div class="ne_gotovo text" id="processing${i}"><p>${value}</p><button id="${i}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${i}">Удалить</button></>`);
+    // }
     tasks[i]={task:value,
         class_name:"ne_gotovo"};
 
@@ -49,32 +49,38 @@ function delete_all(){
 
 function delete_alll() { //удаляет все элементы хронящиеся в main_footer
 delete_all();
-  for (let r=0;r!=i;r++){
+  for (let r=0;r<=i;r++){
     delete tasks[r];
   }
+  i=0;
 }
 
 function filter_all(){
     delete_all();
-    for (let r=0; r!=i; r++){
+    for (let r=0; r<=i; r++){
         main_footer.insertAdjacentHTML('afterbegin',`<div class="${tasks[r].class_name}" id="processing${r}"><p>${tasks[r].task}</p><button id="${r}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${r}">Удалить</button></>`);
     }
 }
 
 function filter_ne_gotovo(){
     delete_all();
-    for (let r=0; r!=i; r++){
+    for (let r=0; r<=i; r++){
         if(tasks[r].class_name=="ne_gotovo"){
                 main_footer.insertAdjacentHTML('afterbegin',`<div class="${tasks[r].class_name}" id="processing${r}"><p>${tasks[r].task}</p><button id="${r}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${r}">Удалить</button></>`);
-        }
+        } else 
+            break;
+        
     }
 }
 
 function filter_gotovo(){
     delete_all();
-    for (let r=0; r!=i; r++){
+    for (let r=0; r<=i; r++){
         if(tasks[r].class_name=="gotovo"){
                 main_footer.insertAdjacentHTML('afterbegin',`<div class="${tasks[r].class_name}" id="processing${r}"><p>${tasks[r].task}</p><button id="${r}" onclick="gotovo(this)" >Выполнена</button><button onClick="delet(this)" id="delete${r}">Удалить</button></>`);
-        }
+        }else
+
+            break;
+
     }
 }
