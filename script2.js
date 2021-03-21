ecf9b02b5bea8fdf1c0d076263497a81d5c45a3d
Fradee2 <donchenko.exceedteam@gmail.com>
2021-03-21 14:07:48 +0300

    Some

--- script2.js
+++ script2.js
@@ -40,11 +40,49 @@ const Unchecked = (parent)=>{
 	}
 }
 
+const delClass=()=>{
+	const $filter_all = document.querySelector('#filter_all').classlist;
+	const $filter_active = document.querySelector('#filter_active').classList;
+	const $filter_completed = document.querySelector('#filter_completed').classList;
+	if ($filter_all =='active'){
+
+		$filter_all.remove('active');
+		$filter_active.remove('active');
+	}
+	if ($filter_active =='active'){
+		$filter_active.remove('active');
+	}else{
+		$filter_completed.remove('active');
+		$filter_active.remove('active');
+	}
+}
+
+const filter=(target,button)=>{
+	delClass();
+	button.classList.add('active')
+	let element = $container.children;
+	console.log(element[1]);
+	for (let i=1; i<=element.length;i++){
+		if (element[i].dataset.checked===target){
+			
+		}else;
+	}
+}
+
 document.addEventListener('click',e=>{
 	const action=e.target.dataset.action;
 	const target=e.target;
 	switch (action){
 		case ACTION.FILTER:{
+			if (target.dataset.filter == 'completed'){
+				filter('false', target);
+			} else if (target.dataset.filter == 'all'){
+				delClass();
+				target.classList.add('active')
+
+			}else if(target.dataset.filter =='active'){
+				filter('true',target);
+			}
 			break;
 		}
 		case ACTION.TOGGLE_COMPLETE:{
